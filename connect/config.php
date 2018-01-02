d24363aa0cac2f16a1a3714bc77cea3b855aec05
DickyQie <18523978916@163.com>
2018-01-02 14:12:53 +0800

    一个PHP写的微博小项目

---
+++ connect/config.php
@@ -0,0 +1,8 @@
+<?php
+//Database 定义
+define('DBHost', 'localhost');  //host地址
+define('DBPort', '3306');       //端口号
+define('DBName', 'weibo');      //数据库用户名
+define('DBUser', 'root');       //数据库密码
+define('DBPassword', 'root');   //数据名
+
---
+++ login.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>登录</title>
+    <!--引入css样式文件-->
+    <link rel="stylesheet" type="text/css" href="public/css/semantic.css" />
+    <link rel="stylesheet" type="text/css" href="public/css/login.css" />
+    <script type="text/javascript" src="public/js/jquery.js"></script>
+    <script type="text/javascript" src="public/static/layer/layer.js"></script>
+</head>
+<body>
+
+<div class="header" title="头部">
+    <img src="public/images/login_logo.png">
+</div>
+
+<div class="main">
+    <div class="left">
+        <div class="login-bg">
+            <img src="public/images/login_banner.png">
+        </div>
+    </div>
+    <div class="content">
+        <!-- 用户输入区开始 -->
+        <div class="ui big form">
+            <div class="ui stacked segment blue">
+                <div class="field">
+                    <div class="ui icon input">
+                        <i class="user icon"></i>
+                        <input type="text" name="username" placeholder="用户名">
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="ui icon input">
+                        <i class="lock icon"></i>
+                        <input type="password" name="password" placeholder="密码">
+                    </div>
+                </div>
+                <button id="login" class="ui fluid large teal submit  button" >登录</button>
+            </div>
+            <div class="ui message">
+                新用户? <a href="">注册</a>
+            </div>
+        </div>
+        <!-- 推荐输入区结束 -->
+
+        <!-- 推荐用户开始 -->
+        <div class="recommend">
+            <div class="ui horizontal divider">
+                <h4 class="ui teal">推荐用户</h4>
+            </div>
+            <div class="ui tiny images">
+                <img class="ui medium circular image" src="public/images/steve_01.png">
+                <img class="ui medium circular image" src="public/images/steve_02.png">
+                <img class="ui medium circular image" src="public/images/steve_03.png">
+                <img class="ui medium circular image" src="public/images/steve_04.png">
+                <img class="ui medium circular image" src="public/images/steve_05.png">
+                <img class="ui medium circular image" src="public/images/steve_06.png">
+                <img class="ui medium circular image" src="public/images/steve_07.png">
+                <img class="ui medium circular image" src="public/images/steve_08.png">
+                <img class="ui medium circular image" src="public/images/steve_09.png">
+            </div>
+        </div>
+        <!-- 推荐用户结束 -->
+    </div>
+</div>
+<div class="clear"></div>
+<div class="footer">
+    Copyright@2016四川省歆语科技有限公司
+</div>
+
+<script>
+    $('#login').click(function(){
+        var username  = $("input[name='username']").val();
+        var password  = $("input[name='password']").val();
+        //js验证用户名和密码
+        if(username == ''){
+            layer.msg('请填写用户名');
+            return false;
+        }
+        if(password == ''){
+            layer.msg('请填写密码');
+            return false;
+        }
+        //js验证
+       
+        return false;
+    });
+</script>
+</body>
+</html>
